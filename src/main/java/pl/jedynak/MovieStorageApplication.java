89c564a978ef771ccfd412b6afea5029f4e598fd
BartoszCoyote <BartoszCoyote@gmail.com>
2017-01-23 10:46:56 +0100

    create new Spring Boot Projct, add pom file

---
+++ src/main/java/pl/jedynak/MovieStorageApplication.java
@@ -0,0 +1,12 @@
+package pl.jedynak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStorageApplication.class, args);
+	}
+}
