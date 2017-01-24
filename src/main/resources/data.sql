cb9055e477d0d3e36a989e9d88be410191ad22dd
BartoszCoyote <BartoszCoyote@gmail.com>
2017-01-24 10:54:57 +0100

    simply fixes, add data.sql for fill database

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO MOVIE VALUES (1,1994,142,'The Shawshank Redemption',9,'Drama','No');
-INSERT INTO MOVIE VALUES (2,1994,10,'tag',11,'tal','nie');
-INSERT INTO MOVIE VALUES (3,1994,10,'tag',11,'tal','nie');
-INSERT INTO MOVIE VALUES (4,1994,10,'tag',11,'tal','nie');
-INSERT INTO MOVIE VALUES (5,1994,10,'tag',11,'tal','nie');
-INSERT INTO MOVIE VALUES (6,1994,10,'tag',11,'tal','nie');
